b76f30d2cfbf540b944e5370b772b753604feb69
Kalpesh Rajput <125110357+Kalpesh-Rajput@users.noreply.github.com>
2025-11-12 16:17:01 +0530

    adding app.py

---
+++ openAiChatbot/app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+import os
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+## Langsmith Tracking
+os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
+
+
+## Prompt Template
+prompt = ChatPromptTemplate(
+    [
+        ("system" , "You are a helpful massistant . Please  repsonse to the user queries"),
+        ("user" , "question:{question}")
+    ]
+)
+
+
+def generate_response(question,api_key,engine,temperature,max_token):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model = engine)
+    output_parser = StrOutputParser()
+    chain = prompt|llm|output_parser
+    answer = chain.invoke({"question" : question})
+    return answer
+
+
+## #Title of the app
+st.title("OpenAI Q&A Chatbot")
+
+
+## Sidebar for settings
+st.sidebar.title("settings")
+api_key = st.sidebar.text_input("Enter Your Open AI Key:" , type="password")
+
+
+## Select the OpenAI model
+engine = st.sidebar.selectbox("Select Open AI model",["gpt-4o","gpt-4-turbo","gpt-4"])
+
+
+## Adjust response parameter
+temperature = st.sidebar.slider("Temperature" , min_value=0.0 , max_value=0.7)
+max_token = st.sidebar.slider("Max Tokens" , min_value=50 , max_value=300 , value=150)
+
+
+
+## MAin interface for user input
+st.write("Go ahead and ask any question")
+user_input = st.text_input("You:")
+
+if user_input and api_key: 
+    response = generate_response(user_input,api_key,engine,temperature,max_token)
+    st.write(response)
+    
+elif user_input:
+    st.warning("Please enter the OPen AI aPi Key in the sider bar")
+elif user_input:
+    st.write("Please provide the user input")
+    
